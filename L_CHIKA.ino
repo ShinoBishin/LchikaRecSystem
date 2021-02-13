f33b6d12086d01ca20772b0d61891cc420759667
kohey0701 <kohey0701@gmail.com>
2021-02-13 19:34:54 +0900

    vscode環境へ移植

--- L_CHIKA.ino
+++ L_CHIKA.ino
@@ -1,3 +1,7 @@
+#include <U8g2lib.h>
+#include <U8x8lib.h>
+
+//必要なヘッダファイルを読み込み
 #include <Arduino.h>
 #include <U8x8lib.h>
 
@@ -5,31 +9,39 @@
 #include <SPI.h>
 #endif
 
+//LEDとボタンを接続した端子を定義
 #define LED 3
 #define BTN1 4
 #define BTN2 5
 
-U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);
+//OLEDディスプレイのライブラリ設定
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE, /* clock=*/SCL, /* data=*/SDA);
 
-void setup() {
-  pinMode(LED,OUTPUT);
-  pinMode(BTN1,INPUT);
-  pinMode(BTN2,INPUT);
+void setup()
+{
+  //各端子を入力、出力に設定
+  pinMode(LED, OUTPUT);
+  pinMode(BTN1, INPUT);
+  pinMode(BTN2, INPUT);
+  //シリアル通信のボーレート指定
   Serial.begin(9600);
+  //OLEDディスプレイ起動
   u8x8.begin();
   u8x8.setFont(u8x8_font_px437wyse700a_2x2_r);
   u8x8.drawString(0, 0, "WELCOME!");
   u8x8.drawString(0, 2, "PUSH1BTN");
-  u8x8.refreshDisplay(); 
+  u8x8.refreshDisplay();
 }
 
+//必要な変数を宣言
+int btn_push[120]; //ボタン入力をタイミング記録する所。0か1しか入らないのでメモリ節約のためにchar型でもよかったかも。
+int mode = 0;      //モードチェンジ時に使います
+int i;             //for文回すのに使います
 
-int btn_push[120];
-int mode = 0;
-int i;
-
-void ready(void){
-  for(i = 0; i < 2; i++){
+void ready(void)
+{
+  for (i = 0; i < 2; i++)
+  {
     digitalWrite(LED, HIGH);
     delay(500);
     digitalWrite(LED, LOW);
@@ -37,10 +49,12 @@ void ready(void){
   }
 }
 
-int btn_read(int x[]){
-  unsigned long now_time = millis()/1000;
+int btn_read(int x[])
+{
+  unsigned long now_time = millis() / 1000;
   i = 0;
-  while(millis()/1000 - now_time < 10){
+  while (millis() / 1000 - now_time < 10)
+  {
     x[i] = digitalRead(BTN2);
     i++;
     delay(100);
@@ -48,64 +62,75 @@ int btn_read(int x[]){
   u8x8.refreshDisplay();
 }
 
-int led_on(int x[]){
-  for (i = 0; i < (sizeof(btn_push)/sizeof(btn_push[0])); i++){
+int led_on(int x[])
+{
+  for (i = 0; i < (sizeof(btn_push) / sizeof(btn_push[0])); i++)
+  {
     delay(100);
-    if(btn_push[i] == 1){
+    if (btn_push[i] == 1)
+    {
       digitalWrite(LED, HIGH);
-    }else{
+    }
+    else
+    {
       digitalWrite(LED, LOW);
     }
   }
   u8x8.refreshDisplay();
 }
 
-void loop() {
-  
-  if(digitalRead(BTN1) == 1){
+void loop()
+{
+
+  if (digitalRead(BTN1) == 1)
+  {
     delay(100);
     mode++;
   }
 
-  switch(mode){
-    case 1:
-      u8x8.drawString(0, 0, "STANDBY!");
-      u8x8.drawString(0, 2, "PUSH1BTN");
-      ready();
-      mode++;
-      break;
-    case 2:
-      if(digitalRead(BTN1) == 1){
-        u8x8.refreshDisplay();
-        mode += 1;
-        }
-        break;
-    case 3:
-      u8x8.drawString(0, 0, "RECMODE!");
-      u8x8.drawString(0, 2, "PUSH2BTN");
-      btn_read(btn_push);
-      u8x8.drawString(0, 0, "STOP!!!!");
-      u8x8.drawString(0, 2, "PUSH1BTN");
-      mode++;
-      break;
-    case 4:
-      if(digitalRead(BTN1) == 1){
-        u8x8.refreshDisplay();
-        mode++;
-        }
-        break;
-    case 5:
-      u8x8.drawString(0, 0, "PLAYBACK");
-      u8x8.drawString(0, 2, "LEDFLASH");
+  switch (mode)
+  {
+  case 1:
+    u8x8.drawString(0, 0, "STANDBY!");
+    u8x8.drawString(0, 2, "PUSH1BTN");
+    ready();
+    mode++;
+    break;
+  case 2:
+    if (digitalRead(BTN1) == 1)
+    {
       u8x8.refreshDisplay();
-      led_on(btn_push);
-      u8x8.drawString(0, 0, "END.....");
-      u8x8.drawString(0, 2, "PUSH1BTN");
-      for (i = 0; i < (sizeof(btn_push)/sizeof(btn_push[0])); i++){
-        btn_push[i] = 0;
-      }
-      mode = 0;
-      break;
+      mode += 1;
+    }
+    break;
+  case 3:
+    u8x8.drawString(0, 0, "RECMODE!");
+    u8x8.drawString(0, 2, "PUSH2BTN");
+    btn_read(btn_push);
+    u8x8.drawString(0, 0, "STOP!!!!");
+    u8x8.drawString(0, 2, "PUSH1BTN");
+    mode++;
+    break;
+  case 4:
+    if (digitalRead(BTN1) == 1)
+    {
+      u8x8.refreshDisplay();
+      mode++;
+    }
+    break;
+  case 5:
+    u8x8.drawString(0, 0, "PLAYBACK");
+    u8x8.drawString(0, 2, "LEDFLASH");
+    u8x8.refreshDisplay();
+    led_on(btn_push);
+    u8x8.drawString(0, 0, "END.....");
+    u8x8.drawString(0, 2, "PUSH1BTN");
+    for (i = 0; i < (sizeof(btn_push) / sizeof(btn_push[0])); i++)
+    {
+      btn_push[i] = 0;
+    }
+    mode = 0;
+    break;
   }
   delay(500);
 }
